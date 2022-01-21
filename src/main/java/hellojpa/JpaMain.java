82a7aa521bbdfc478c79e40b0b0f8ff744adae08
duaghwns <hj2758@gmail.com>
2022-01-21 23:54:21 +0900

    엔티티 생성

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import jpabook.jpashop.domain.Member;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
